a72055176cfba4e0d84598e1eb4b2a76013ef96d
barneycarroll <barney.carroll@gmail.com>
2016-08-18 17:03:13 +0100

    First push

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # muty
+
 An attempt at a more intuitive API wrapper around W3C DOM MutationObserver
+
+```javascript
+var nodeObserver = muty( {
+	childList : true,
+	subtree   : true
+},
+	document,
+	mutations => {
+		// ...
+	}
+)
+```
+
+For convenience, an object with all valid boolean options set to `true` is included as `muty.options`. This allows blanket coverage via:
+
+```javascript
+muty( muty.options, document, callback )
+```
---
+++ index.js
@@ -0,0 +1,32 @@
+function muty( options, target, callback ){
+	const observer = new MutationObserver( callback )
+
+	observer.observe( target, options )
+
+	return observer
+}
+
+muty.options = {
+	childList             : true
+	,
+	attributes            : true
+	,
+	characterData         : true
+	,
+	subtree               : true
+	,
+	attributeOldValue     : true
+	,
+	characterDataOldValue : true
+	// ,
+	// attributeFilter    : []
+}
+
+if (typeof module == 'object' )
+	module.exports = muty
+
+else if (typeof define == 'function')
+	define( function(){ return muty })
+
+else if (typeof window == 'object')
+	window.muty = muty
