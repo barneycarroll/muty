268419ad3b58fd777a33d9548e1d3e3ed8f63acb
Barney Carroll <barney.carroll@gmail.com>
2016-08-19 11:57:25 +0100

    Add unit test to API tests, patch bump

--- tests.js
+++ tests.js
@@ -1,7 +1,7 @@
 var tape = require( 'tape' )
 var muty = require( './index' )
 
-tape( 'Provides an options hash at `muty.options`', function( t ){
+tape( 'Provides a valid options hash at `muty.options`', function( t ){
 	t.equal( typeof muty.options, 'object' )
 
 	t.end()
@@ -19,4 +19,22 @@ tape( 'Returns a MutationObserver', function( t ){
 	t.ok( muty( muty.options, document, function(){} ) instanceof MutationObserver )
 
 	t.end()
+} )
+
+tape( 'Provided default options, invokes callback after any DOM mutation', function( t ){
+	var called = false
+
+	muty( muty.options, document, function(){
+		called = true
+	} )
+
+	t.equals( called, false )
+
+	document.body.appendChild( document.createTextNode( '' ) )
+
+	setTimeout( function(){
+		t.equals( called, true )
+
+		t.end()
+	} )
 } )
